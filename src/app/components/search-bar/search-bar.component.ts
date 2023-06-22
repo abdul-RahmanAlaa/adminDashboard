a1fd28ea50f4507019ab201bfbd88843b3103d3b
abdo <abdoalaaabdo17@gmail.com>
2023-06-22 19:56:01 +0300

    search bar static

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
-
+  alphabets: string[] = [];
+  ngOnInit() {
+    for (let i = 65; i <= 90; i++) {
+      this.alphabets.push(String.fromCharCode(i));
+    }
+  }
 }
